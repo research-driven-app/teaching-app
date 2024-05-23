b2d68e85a1c6b71d76a9caba7ecbf5f2a976faff
Paolo Tamagnini <paolotamag@gmail.com>
2024-05-23 18:21:13 +0200

    first_version

--- app.py
+++ app.py
@@ -1,3 +1,69 @@
 import streamlit as st
+import pandas as pd
 
-st.write("Paolo makes data apps in Python now")
+st.title("Brand Reputation App")
+st.markdown(" ")
+
+# Create three columns
+col1, spacer, col2 = st.columns([1.2,0.1,0.8])
+
+uploaded_file = col1.file_uploader("Choose a CSV file", type=("csv"))
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    col1.write("Tweets Data Uploaded")
+else:
+    df = pd.read_csv("data/default_clean.csv")
+
+# Add a horizontal line
+col1.markdown("---")
+
+col11, spacer11, col12 = col1.columns([1,0.1,1])
+
+default_dictionary = pd.read_excel("data/default_dict.xlsx")
+
+# Add a download button for the Excel file
+
+svg_string = """
+<center>
+<svg width="50px" height="50px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000" version="1.1" id="Capa_1" width="800px" height="800px" viewBox="0 0 395.001 395" xml:space="preserve">
+<g>
+	<g>
+		<path d="M322.852,0H72.15C32.366,0,0,32.367,0,72.15v250.7C0,362.634,32.367,395,72.15,395h250.701    c39.784,0,72.15-32.366,72.15-72.15V72.15C395.002,32.367,362.635,0,322.852,0z M370.002,322.85    c0,25.999-21.151,47.15-47.15,47.15H72.15C46.151,370,25,348.849,25,322.85V72.15C25,46.151,46.151,25,72.15,25h250.701    c25.999,0,47.15,21.151,47.15,47.15L370.002,322.85L370.002,322.85z"/>
+		<path d="M197.501,79.908c-33.775,0-61.253,27.479-61.253,61.254c0,6.903,5.596,12.5,12.5,12.5c6.904,0,12.5-5.597,12.5-12.5    c0-19.99,16.263-36.254,36.253-36.254s36.253,16.264,36.253,36.254c0,11.497-8.314,19.183-22.01,30.474    c-12.536,10.334-26.743,22.048-26.743,40.67v40.104c0,6.902,5.597,12.5,12.5,12.5c6.903,0,12.5-5.598,12.5-12.5v-40.104    c0-6.832,8.179-13.574,17.646-21.381c13.859-11.426,31.106-25.646,31.106-49.763C258.754,107.386,231.275,79.908,197.501,79.908z"/>
+		<path d="M197.501,283.024c-8.842,0-16.034,7.193-16.034,16.035c0,8.84,7.192,16.033,16.034,16.033    c8.841,0,16.034-7.193,16.034-16.033C213.535,290.217,206.342,283.024,197.501,283.024z"/>
+	</g>
+</g>
+</svg>
+</center>
+"""
+
+info_string = "You can upload an XLSX file with 3 columns and the same amount of rows of the current dictionary. &#013; Download the current dictionary to generate such data."
+
+col11.markdown("<span title='" + info_string + "'>" + svg_string + "</span>", unsafe_allow_html=True)
+
+col12.download_button(
+    label="Default Dictionary",
+    data=df.to_csv(index=False).encode(),
+    file_name="default_dict.csv",
+    mime="text/csv",
+)
+
+# Add a switch to the left column
+if col12.checkbox("Add Additional Diver"):
+    uploaded_file2 = col1.file_uploader("Upload Extra Dictionary (Additional Driver)", type=("xlsx"))
+
+    if uploaded_file2 is not None:
+        df_add = pd.read_excel(uploaded_file2)
+        col1.write("Additional Driver Dictionary Uploaded")
+    else:
+        df_add = pd.read_excel("data/default_additional_dictionary.xlsx")
+
+# Add a string input to the right column with a default value
+string_input = col2.text_input("Timestamp Format", "yyyy-MM-dd'T'HH:mm:ss")
+
+# Add three column selections to the right column with default values
+col_selection1 = col2.selectbox("Select ID", df.columns, index=df.columns.get_loc('tweet_id'))
+col_selection2 = col2.selectbox("Select Timestamp", df.columns, index=df.columns.get_loc('created_at'))
+col_selection3 = col2.selectbox("Select Text", df.columns, index=df.columns.get_loc('text'))
+
+st.table(df.head())
